60ba18c37cd3af3d47efc8fa5b5b9b3fdd508375
Hong Sung <profsung@gmail.com>
2017-09-06 15:17:24 -0500

    updated style

--- app/src/main/java/edu/uco/hsung/m05_fragmentcommunication/SelectionFragment.java
+++ app/src/main/java/edu/uco/hsung/m05_fragmentcommunication/SelectionFragment.java
@@ -26,6 +26,11 @@ public class SelectionFragment extends Fragment {
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_selection, container, false);
+    }
+
+    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
@@ -44,11 +49,6 @@ public class SelectionFragment extends Fragment {
                         spinnerTopping2.getSelectedItem().toString());
             }
         });
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_selection, container, false);
     }
 
 }
